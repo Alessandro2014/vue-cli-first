b62e254658c39bc175dae28b30797a581c5e1c30
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-03 17:31:33 +0200

    components Title

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1 {
+  color: yellowgreen;
+}
+</style>
